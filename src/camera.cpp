c716277343fdd141df692adcae3a93ea03ed2043
srane96 <srane96@terpmail.umd.edu>
2018-12-06 13:19:25 -0500

    [#6: ROBO] Implement Camera class that subscribes to image_raw topic to get an image of the environment. Based on the image read, this class can detect presence of an exit sign and also interpret direction from it.

--- src/camera.cpp
+++ src/camera.cpp
@@ -34,32 +34,128 @@
  */
 #include "../include/camera.h"
 Camera::Camera() {
+  signDetected = false;
+  nowTurn = 0;
+  count = 0;
+  countB = 0;
   /// Subscribe to /camera/rgb/image_raw topic
-  subImage = nh.subscribe < sensor_msgs::Image
+    subImage = nh.subscribe < sensor_msgs::Image
         > ("/camera/rgb/image_raw", 10, &Camera::checkImage, this);
 }
 Camera::~Camera() {
+
 }
 void Camera::checkImage(const sensor_msgs::Image::ConstPtr& dataImage) {
-  /// Camera data, code for using this data for image processing
+  try {
+      cv_ptr = cv_bridge::toCvCopy(dataImage, sensor_msgs::image_encodings::BGR8);
+    } catch (cv_bridge::Exception &e) {
+    //ROS_ERROR("cv_bridge exception: %s", e.what());
+    return;
+    }
+    lowerRed = cv::Scalar(0, 70, 50);
+    higherRed = cv::Scalar(10, 255, 250);
+    lowerGreen = cv::Scalar(45, 100, 50);
+    higherGreen = cv::Scalar(65, 255, 255);
+    lowerBlue = cv::Scalar(110, 50, 50);
+    higherBlue = cv::Scalar(130, 255, 255);
+    cv::Mat imageHSV;
+    cv::Mat mask;
+    cv::Mat res;
+    cv::Mat res_gray;
+    cv::Mat thresh;
+    cv::cvtColor(cv_ptr->image, imageHSV, cv::COLOR_BGR2HSV);
+    cv::inRange(imageHSV, lowerGreen, higherGreen, mask);
+    cv::bitwise_and(imageHSV, imageHSV, res, mask);
+    cv::cvtColor(res, res_gray, CV_BGR2GRAY);
+    cv::threshold(res_gray, thresh, 40, 255, cv::THRESH_BINARY);
+    for (int i = 0; i < thresh.size().height; i++) {
+      for (int j = 0; j < thresh.size().width; j++) {
+        if (thresh.at<int>(i, j) == 255) {
+          signDetected = true;
+          if (i < 2) {
+            count  = count + 1;
+            if(count > 45){
+             count = 0;
+             nowTurn = 5;
+            }
+          }
+        //ROS_INFO("I can see the green sign %d", count);
+        }
+      }
+    }
+    cv::Mat imageHSVB;
+    cv::Mat maskB;
+    cv::Mat resB;
+    cv::Mat res_grayB;
+    cv::Mat threshB;
+    cv::cvtColor(cv_ptr->image, imageHSVB, cv::COLOR_BGR2HSV);
+    cv::inRange(imageHSVB, lowerBlue, higherBlue, maskB);
+    cv::bitwise_and(imageHSVB, imageHSVB, resB, maskB);
+    cv::cvtColor(resB, res_grayB, CV_BGR2GRAY);
+    cv::threshold(res_grayB, threshB, 40, 255, cv::THRESH_BINARY);
+    for (int i = 0; i < threshB.size().height; i++) {
+      for (int j = 0; j < threshB.size().width; j++) {
+        if (threshB.at<int>(i, j) == 255) {
+          signDetected = true;
+          if (i < 2) {
+            countB  = countB + 1;
+            if(countB > 45){
+             countB = 0;
+             nowTurn = 10;
+            }
+          }
+        //ROS_INFO("I can see the blue sign %d", countB);
+          break;
+        }
+      }
+    }
+    //ROS_INFO_STREAM("dir =  "<<direction << "nowTurn = " << nowTurn);
+    cv::Mat imageHSVR;
+    cv::Mat maskR;
+    cv::Mat resR;
+    cv::Mat res_grayR;
+    cv::Mat threshR;
+    cv::cvtColor(cv_ptr->image, imageHSVR, cv::COLOR_BGR2HSV);
+    cv::inRange(imageHSVR, lowerRed, higherRed, maskR);
+    cv::bitwise_and(imageHSVR, imageHSVR, resR, maskR);
+    cv::cvtColor(resR, res_grayR, CV_BGR2GRAY);
+    cv::threshold(res_grayR, threshR, 40, 255, cv::THRESH_BINARY);
+    for (int i = 0; i < threshR.size().height; i++) {
+      for (int j = 0; j < threshR.size().width; j++) {
+        if (threshR.at<int>(i, j) == 255) {
+          signDetected = true;
+          if (i < 2) {
+            nowTurn = 15;
+          }
+        //ROS_INFO("I can see the red sign");
+          break;
+        }
+      }
+    }
+    cv::imshow("Window", imageHSVB);
+    cv::waitKey(3);
 }
 int Camera::getNowTurn() {
-  return 1;
+  return nowTurn;
 }
 int Camera::getCount() {
-  return 1;
+  return count;
 }
 int Camera::getCountB() {
-  return 1;
+  return countB;
 }
 bool Camera::getSignDetected() {
-  return false;
+  return signDetected;
 }
 void Camera::setNowTurn(int turn) {
+  nowTurn = turn;
 }
 void Camera::setCount(int c) {
+  count = c;
 }
 void Camera::setCountB(int cB) {
+  countB = cB;
 }
 void Camera::setSignDetected(bool sD) {
+  signDetected = sD;
 }
